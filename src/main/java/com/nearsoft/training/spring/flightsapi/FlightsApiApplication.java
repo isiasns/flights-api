e13217feba19c079ed3055c271e7c966faf4b7ba
Isaac Sias <isias@nearsoft.com>
2018-10-23 16:39:55 -0600

    Initial project structure

---
+++ src/main/java/com/nearsoft/training/spring/flightsapi/FlightsApiApplication.java
@@ -0,0 +1,12 @@
+package com.nearsoft.training.spring.flightsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/nearsoft/training/spring/flightsapi/FlightsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nearsoft.training.spring.flightsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightsApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
