0d6f814e9bed6a5ae352c43758cdbefa0a71e5ee
Isaac Sias <isias@nearsoft.com>
2018-10-26 16:23:11 -0600

    Changed strings to string builder

--- src/main/java/com/nearsoft/training/spring/flightsapi/util/ApiUtil.java
+++ src/main/java/com/nearsoft/training/spring/flightsapi/util/ApiUtil.java
@@ -8,40 +8,59 @@ import java.util.Map;
 @Component
 public class ApiUtil {
     private FlightsApiConfiguration flightsApiConfiguration;
+    private final static String SLASH = "/";
+    private final static String QUESTION = "?";
+    private final static String EQUALS = "=";
+    private final static String AMP = "&";
 
     public ApiUtil(FlightsApiConfiguration flightsApiConfiguration) {
         this.flightsApiConfiguration = flightsApiConfiguration;
     }
 
     public String getApiUrl(String api, Map<String, String> requiredParams) {
-        StringBuilder required = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
+        builder.append(flightsApiConfiguration.getUri());
+        builder.append(SLASH);
+        builder.append(api);
+        builder.append(SLASH);
+        builder.append(flightsApiConfiguration.getProtocol());
+        builder.append(SLASH);
+        builder.append(flightsApiConfiguration.getVersion());
+        builder.append(SLASH);
+        builder.append(flightsApiConfiguration.getFormat());
         if (requiredParams != null) {
             for (Map.Entry<String, String> parameter : requiredParams.entrySet()) {
-                required.append("/");
-                required.append(parameter.getKey());
+                builder.append(SLASH);
+                builder.append(parameter.getKey());
                 if (!parameter.getValue().isEmpty()) {
-                    required.append("/");
-                    required.append(parameter.getValue());
+                    builder.append(SLASH);
+                    builder.append(parameter.getValue());
                 }
             }
         }
-        return flightsApiConfiguration.getUri() + "/" + api + "/" + flightsApiConfiguration.getProtocol() + "/" + flightsApiConfiguration.getVersion() + "/" + flightsApiConfiguration.getFormat() + required.toString() + "?"
-                + "appId=" + flightsApiConfiguration.getId() + "&" + "appKey=" + flightsApiConfiguration.getKey();
+        builder.append(QUESTION);
+        builder.append("appId=");
+        builder.append(flightsApiConfiguration.getId());
+        builder.append(AMP);
+        builder.append("appKey=");
+        builder.append(flightsApiConfiguration.getKey());
+        return  builder.toString();
     }
 
     public String getApiUrl(String api, Map<String, String> requiredParams, Map<String, String> optionalParams) {
         String apiUrl = getApiUrl(api, requiredParams);
-        StringBuilder optional = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
+        builder.append(apiUrl);
         if (optionalParams != null) {
             for (Map.Entry<String, String> parameter : optionalParams.entrySet()) {
                 if (!parameter.getValue().isEmpty()) {
-                    optional.append("&");
-                    optional.append(parameter.getKey());
-                    optional.append("=");
-                    optional.append(parameter.getValue());
+                    builder.append(AMP);
+                    builder.append(parameter.getKey());
+                    builder.append(EQUALS);
+                    builder.append(parameter.getValue());
                 }
             }
         }
-        return apiUrl + optional.toString();
+        return builder.toString();
     }
 }
